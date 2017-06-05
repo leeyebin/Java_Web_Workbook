12861b0870747fbd2157859b090c3e9f21629a7e
leeyebin <leeyebin007@gmail.com>
2017-06-05 18:06:01 +0900

    실력향상문제

---
+++ web05/src/spms/vo/Project.java
@@ -0,0 +1,112 @@
+package spms.vo;
+
+import java.util.Date;
+
+public class Project {
+	protected int no;
+	protected String title;
+	protected String content;
+	protected Date startDate;
+	protected Date endDate;
+	protected int state;
+	protected Date createdDate;
+	protected String tags;
+	/**
+	 * @return the no
+	 */
+	public int getNo() {
+		return no;
+	}
+	/**
+	 * @param no the no to set
+	 */
+	public void setNo(int no) {
+		this.no = no;
+	}
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	/**
+	 * @return the content
+	 */
+	public String getContent() {
+		return content;
+	}
+	/**
+	 * @param content the content to set
+	 */
+	public void setContent(String content) {
+		this.content = content;
+	}
+	/**
+	 * @return the starDate
+	 */
+	public Date getStartDate() {
+		return startDate;
+	}
+	/**
+	 * @param starDate the starDate to set
+	 */
+	public void setStartDate(Date starDate) {
+		this.startDate = starDate;
+	}
+	/**
+	 * @return the endDate
+	 */
+	public Date getEndDate() {
+		return endDate;
+	}
+	/**
+	 * @param endDate the endDate to set
+	 */
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	/**
+	 * @return the state
+	 */
+	public int getState() {
+		return state;
+	}
+	/**
+	 * @param state the state to set
+	 */
+	public void setState(int state) {
+		this.state = state;
+	}
+	/**
+	 * @return the createdDate
+	 */
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+	/**
+	 * @param createdDate the createdDate to set
+	 */
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+	/**
+	 * @return the tags
+	 */
+	public String getTags() {
+		return tags;
+	}
+	/**
+	 * @param tags the tags to set
+	 */
+	public void setTags(String tags) {
+		this.tags = tags;
+	}
+
+	
+}
