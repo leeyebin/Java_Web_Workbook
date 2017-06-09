c9490313e8a4cbb7f5f00aee6b46eec4af17c0a2
leeyebin <leeyebin007@gmail.com>
2017-06-09 16:50:27 +0900

    Java web workbook p566

--- web05/src/spms/dao/ProjectDao.java
+++ web05/src/spms/dao/ProjectDao.java
@@ -5,5 +5,9 @@ import java.util.List;
 import spms.vo.Project;
 
 public interface ProjectDao {
-	List<Project> selectList() throws Exception;
+  List<Project> selectList() throws Exception;
+  int insert(Project project) throws Exception;
+  Project selectOne(int no) throws Exception;
+  int update(Project project) throws Exception;
+  int delete(int no) throws Exception;
 }
