bbe3601fb033dbd7b4f8d673d644a29b2bba66d8
leeyebin <leeyebin007@gmail.com>
2017-06-26 17:47:06 +0900

    Java web workbook p652

--- web05/src/spms/controls/MemberListController.java
+++ web05/src/spms/controls/MemberListController.java
@@ -1,28 +1,34 @@
 package spms.controls;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import spms.annotation.Component;
+import spms.bind.DataBinding;
 import spms.dao.MySqlMemberDao;
 
 @Component("/member/list.do")
-public class MemberListController implements Controller{
+public class MemberListController implements Controller, DataBinding {
 	MySqlMemberDao memberDao;
-	//MemberDao를 주입 받기 위한 인스턴스 변수와 setter 메서드를 추가함.
-	public MemberListController setMemberDao(MySqlMemberDao memberDao){
+
+	// MemberDao를 주입 받기 위한 인스턴스 변수와 setter 메서드를 추가함.
+	public MemberListController setMemberDao(MySqlMemberDao memberDao) {
 		this.memberDao = memberDao;
 		return this;
 	}
-	
+
+	@Override
+	public Object[] getDataBinders() {
+		return new Object[] { "orderCond", String.class };
+	}
+
 	@Override
 	public String execute(Map<String, Object> model) throws Exception {
-		//외부에서 MemberDao 객체를 주입해 줄 것이기 때문에 이제 더 이상 Map 객체에서 MemberDao를 꺼낼 필요가 없다.
-		//MemberDao memberDao = (MemberDao)model.get("memberDao");
-		
-		//페이지 컨트롤러가 작업한 결과물을 Map에 담기
-		System.out.println(memberDao);
-		model.put("members",  memberDao.selectList());
-		//뷰 URL 반환
+		HashMap<String, Object> paramMap = new HashMap<String, Object>();
+		paramMap.put("orderCond", model.get("orderCond"));
+
+		model.put("members", memberDao.selectList(paramMap));
 		return "/member/MemberList.jsp";
 	}
+
 }
--- web05/src/spms/temp/MemberListServlet.java
+++ web05/src/spms/temp/MemberListServlet.java
@@ -1,4 +1,4 @@
-package spms.temp;
+/*package spms.temp;
 
 import java.io.IOException;
 
@@ -26,16 +26,17 @@ public class MemberListServlet extends HttpServlet {
 			request.setAttribute("members", memberDao.selectList());
 			request.setAttribute("viewUrl", "/member/MemberList.jsp"); //JSP URL 정보를 프런트 컨트롤러에게 알려주고자 ServletRequest 보관소에 저장
 			
-			/*response.setContentType("text/html; charset=UTF-8"); // 응답 데이터의 문자 집합은 프런트 컨트롤러에서 이미 설정
+			response.setContentType("text/html; charset=UTF-8"); // 응답 데이터의 문자 집합은 프런트 컨트롤러에서 이미 설정
 			RequestDispatcher rd = request.getRequestDispatcher("/member/MemberList.jsp"); //화면 출력을 위해 JSP로 실행을 위임하는 것도 프런트 컨트롤러가 처리
-			rd.include(request,  response);*/
+			rd.include(request,  response);
 			
 		} catch (Exception e) {
 			throw new ServletException(e); //service() 메서드는 ServletException을 던지도록 선언되어 있기 때문에 기존의 예외 객체를 그냥 던질 수 없다. 그래서 ServletException 객체를 생성한다.
-			/*e.printStackTrace(); //오류가 발생했을 때 오류 처리 페이지로 실행을 위임하는 작업도 프런트 컨트롤러가 한다.
+			e.printStackTrace(); //오류가 발생했을 때 오류 처리 페이지로 실행을 위임하는 작업도 프런트 컨트롤러가 한다.
 			request.setAttribute("error", e);
 			RequestDispatcher rd = request.getRequestDispatcher("/Error.jsp");
-			rd.forward(request, response);*/
+			rd.forward(request, response);
 		}
 	}
 }
+*/
